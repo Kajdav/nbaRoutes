cf530ecac383fa70c21fd78081e946f6b54f2a64
kajdav <poppa.bear.joe@gmail.com>
2014-09-29 17:09:12 -0600

    Adds angularJS framework. Completes step 1 halfway through step 4.  See ReadME for more info.

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,14 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
-
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
+	console.log($scope.teamData);
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
+	function $scope.toggleNewGameForm() {
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+	}
+	if ($routeParams === 'utahjazz') {
+		
+	}
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,41 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
-
+	this.addNewGame = function(gameObject) {
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		if (parseInt(gameObj.homeTeamScore) >= parseInt(gameObj.opponentScore)) {
+		    gameObj.won = true;
+		} else {
+		    gameObj.won = false;
+		}
+		return $http({
+			method: 'POST',
+			data: gameObj,
+			url: url
+		})
+	}
+	this.getTeamData = function(team) {
+		var deferred = $q.defer();
+		$http({
+			method: 'GET',
+			url: "https://api.parse.com/1/classes/" + team
+		})
+			.then(function(data){
+				var results = data.data.results;
+				var wins = 0;
+				var losses = 0;
+				for (var i = 0; i < results.length; i++) {
+					if (results[i].won = true) {
+						wins++;
+					}
+					else {
+						losses++;
+					}
+				}
+				results.wins = wins;
+				results.losses = losses;
+				deferred.resolve(results);
+			})
+		return deferred.promise;
+	}
 });
